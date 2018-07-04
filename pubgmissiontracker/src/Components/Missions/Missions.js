25374053db48d8a02727877660fcc7183ccfba0b
raynerprogramming <raynerprogramming@gmail.com>
2018-07-03 22:20:24 -0500

    Accordion events

--- pubgmissiontracker/src/Components/Missions/Missions.js
+++ pubgmissiontracker/src/Components/Missions/Missions.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { Card, Image, Button, List } from 'semantic-ui-react'
+import { Card, Image, Button, List, Grid } from 'semantic-ui-react'
 class Missions extends React.Component {
-    render() {
+    state = {
+        list: this.props.list
+    }
+    handleClick = (e, titleProps) => {
+        this.setState({ completed: !titleProps.mission.completed })
+    }
+    toggleMission = (index) =>{
+        var newVal = !this.state.list.Missions[index].completed
+        var newList = this.state.list
+        newList.Missions[index].completed = newVal
+        this.setState({list:newList})
+    }
+    getStyle(mission){
+        return {
+            cursor: 'pointer',
+            'background-color': mission.completed ? 'green' : 'red'
+           }
+    }
+    render() {       
+    
         var imgStyle = {
             "width": "100%"
         };
@@ -13,21 +32,22 @@ class Missions extends React.Component {
             "left": "46%",
             "transform": "translate(-50%, -50%)",
             "font-size": "10vh"
-        }
-        
-        var list = this.props.list;
+        }        
         return (
             <List divided relaxed>
                 {
-                    list.Missions.map  (function(mission,index){
+                    this.state.list.Missions.map(function (mission, index) {
                         return <List.Item>
-                                    <List.Icon name='github' size='large' verticalAlign='middle' />
-                                    <List.Content>
-                                        <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
-                                        <List.Description as='a'>{mission.xp}</List.Description>
-                                    </List.Content>
-                                </List.Item>
-                    })
+                            <div style={this.getStyle(mission)} onClick={() => this.toggleMission(index)}>
+                                <Grid stretched centered>
+                                    <Grid.Row centered>
+                                        <Grid.Column width={15}>{mission.text}</Grid.Column>
+                                        <Grid.Column width={1}>{mission.xp}</Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </div>
+                        </List.Item>
+                    },this)
                 }
             </List>
         )
